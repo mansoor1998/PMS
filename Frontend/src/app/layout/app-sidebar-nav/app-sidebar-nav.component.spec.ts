2604ff902809706dde74f67ae9478a6e996e5a7b
Mansoor Ali Soomro <mk.soomro@yahoo.com>
2021-10-10 16:33:29 +0200

    update the project

--- Frontend/src/app/layout/app-sidebar-nav/app-sidebar-nav.component.spec.ts
+++ Frontend/src/app/layout/app-sidebar-nav/app-sidebar-nav.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppSidebarNavComponent } from './app-sidebar-nav.component';
 
@@ -6,7 +6,7 @@ describe('AppSidebarNavComponent', () => {
   let component: AppSidebarNavComponent;
   let fixture: ComponentFixture<AppSidebarNavComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AppSidebarNavComponent ]
     })
--- Frontend/src/shared/components/pre-loader/pre-loader.component.spec.ts
+++ Frontend/src/shared/components/pre-loader/pre-loader.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PreLoaderComponent } from './pre-loader.component';
 
@@ -6,7 +6,7 @@ describe('PreLoaderComponent', () => {
   let component: PreLoaderComponent;
   let fixture: ComponentFixture<PreLoaderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PreLoaderComponent ]
     })
